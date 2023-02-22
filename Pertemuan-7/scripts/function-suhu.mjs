1f17c4a69f728dfdaffea338d056d54c401bca9d
Juan Salainti <116140732+jmichaes21@users.noreply.github.com>
2023-02-23 07:31:01 +0800

    Delete Pertemuan-7 directory

--- Pertemuan-7/scripts/function-suhu.mjs
+++
@@ -1,6 +0,0 @@
-// Suhu conversion Fahrenheit to Celcius
-
-export let fahrenheitToCelcius = (fahrenheit) => {
-    let celcius = 5 / 9 * (fahrenheit - 32);
-    console.log(`${fahrenheit} fahrenheit = ${celcius} celcius`);
-};
