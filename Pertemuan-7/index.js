2ff4c2c6e1ed330b2bf385f82e179fad54007313
Juan Salainti <116140732+jmichaes21@users.noreply.github.com>
2023-02-23 07:30:30 +0800

    Delete Pertemuan-7 directory

--- Pertemuan-7/index.js
+++
@@ -1,6 +0,0 @@
-// import all from scripts
-import { fahrenheitToCelcius } from "./scripts/function-suhu.mjs";
-import BMIcounter from "./scripts/function-BMI.mjs";
-
-fahrenheitToCelcius(180);
-BMIcounter(180, 60);
--- Pertemuan-7/scripts/function-BMI.mjs
+++
@@ -1,8 +0,0 @@
-// BMI Function
-
-let BMIcounter = (tinggi, berat) => {
-    let result = berat / (tinggi * tinggi);
-    console.log(`Hasil dari BMI anda adalah : ${result}`)
-};
-
-export default BMIcounter;
